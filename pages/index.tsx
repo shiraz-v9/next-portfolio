0d22de6d868b5c0ec1a65266bbed0a5bfd1ffd08
shiraz-v9 <shiraz.xt@gmail.com>
2022-12-23 17:25:57 +0000

    fixxed build errs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
-const Homepage = dynamic(() => import("../components/Homepage"), {
+const Homepage = dynamic(() => import("../components/homepage"), {
   ssr: false,
 });
 
